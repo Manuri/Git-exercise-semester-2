2b256d6058720beb6e634d139354b1f427f24e1e
Manuri <manuriamaya@hotmail.com>
2012-01-19 11:10:16 +0530

    Update workspace/Project1/src/Package1/Guitarist.java

--- workspace/Project1/src/Package1/Guitarist.java
+++ workspace/Project1/src/Package1/Guitarist.java
@@ -2,6 +2,10 @@ package Package1;
 
 public class Guitarist extends Musician{
   
+  Guitarist(String aName) {
+    super(aName);
+  }
+  
   public void play(){
     system.out.println("Playing the guitar");
   }
